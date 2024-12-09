8dd88c62fdb4a5536d809b37bcdd3003a7422a4b
Danny Seymour <danny@seymour.family>
2024-12-08 23:02:28 -0800

    fix: Add Backblaze creds to namespaces that need it

--- hack/generate-secrets.sh
+++ hack/generate-secrets.sh
@@ -9,18 +9,20 @@ kubeseal -f secrets/synapse/bridges/registrations/secret.yaml -o yaml | grep -v 
 kubeseal -f secrets/redis/secret.yaml -o yaml | grep -v creationTimestamp >manifests/redis/sealedsecret.yaml
 kubeseal -f secrets/immich/secret.yaml -o yaml | grep -v creationTimestamp >manifests/immich/sealedsecret.yaml
 kubeseal -f secrets/inadyn/secret.yaml -o yaml | grep -v creationTimestamp >manifests/inadyn/sealedsecret.yaml
-kubeseal -f secrets/postgres/secret.yaml -n immich -o yaml | grep -v creationTimestamp >>manifests/immich/sealedsecret.yaml
 kubeseal -f secrets/cert-manager/secret.yaml -o yaml | grep -v creationTimestamp >manifests/cert-manager/sealedsecret.yaml
 kubeseal -f secrets/nextcloud/secret.yaml -o yaml | grep -v creationTimestamp >manifests/nextcloud/sealedsecret.yaml
 kubeseal -f secrets/postgres/secret.yaml -n nextcloud -o yaml | grep -v creationTimestamp >>manifests/nextcloud/sealedsecret.yaml
 kubeseal -f secrets/synapse/matrix-synapse.yaml -o yaml | grep -v creationTimestamp >manifests/synapse/sealedsecret.yaml
-echo "---" >>manifests/synapse/sealedsecret.yaml
+# echo "---" >>manifests/synapse/sealedsecret.yaml
 kubeseal -f secrets/synapse/matrix-synapse-signingkey.yaml -o yaml | grep -v creationTimestamp >>manifests/synapse/sealedsecret.yaml
-echo "---" >>manifests/synapse/sealedsecret.yaml
+# echo "---" >>manifests/synapse/sealedsecret.yaml
 kubeseal -f secrets/synapse/postgres-synapse.yaml -o yaml | grep -v creationTimestamp >>manifests/synapse/sealedsecret.yaml
-echo "---" >>manifests/synapse/sealedsecret.yaml
+# echo "---" >>manifests/synapse/sealedsecret.yaml
 kubeseal -f secrets/synapse/redis-password.yaml -o yaml | grep -v creationTimestamp >>manifests/synapse/sealedsecret.yaml
-echo "---" >>manifests/synapse/sealedsecret.yaml
+# echo "---" >>manifests/synapse/sealedsecret.yaml
 kubeseal -f secrets/synapse/sliding-sync.yaml -o yaml | grep -v creationTimestamp >>manifests/synapse/sealedsecret.yaml
 kubeseal -f secrets/velero/credentials-velero.yaml -o yaml | grep -v creationTimestamp >manifests/velero/sealedsecret.yaml
 kubeseal -f secrets/velero/velero-repo-credentials.yaml -o yaml | grep -v creationTimestamp >>manifests/velero/sealedsecret.yaml
+kubeseal -f secrets/postgres/secret.yaml -n immich -o yaml | grep -v creationTimestamp >>manifests/immich/sealedsecret.yaml
+kubeseal -f secrets/postgres/secret.yaml -n authentik -o yaml | grep -v creationTimestamp >>manifests/authentik/sealedsecret.yaml
+kubeseal -f secrets/postgres/secret.yaml -n synapse -o yaml | grep -v creationTimestamp >>manifests/synapse/sealedsecret.yaml
