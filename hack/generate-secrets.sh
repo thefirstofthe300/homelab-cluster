2ff891a9aab4ac2b307dda71a4eaeeb54feb1500
Danny Seymour <danny@seymour.family>
2025-07-12 22:17:21 -0700

    feat: Migrate to ESS Helm chart

--- hack/generate-secrets.sh
+++ hack/generate-secrets.sh
@@ -1,21 +1,23 @@
 #! /usr/bin/env bash
 
-kubeseal -f secrets/authentik/secret.yaml -o yaml | grep -v creationTimestamp >manifests/authentik/sealedsecret.yaml
-kubeseal -f secrets/synapse/bridges/mautrix-signal/secret.yaml -o yaml | grep -v creationTimestamp >manifests/synapse/bridges/mautrix-signal/sealedsecret.yaml
-kubeseal -f secrets/synapse/bridges/mautrix-gmessages/secret.yaml -o yaml | grep -v creationTimestamp >manifests/synapse/bridges/mautrix-gmessages/sealedsecret.yaml
-kubeseal -f secrets/synapse/bridges/mautrix-telegram/secret.yaml -o yaml | grep -v creationTimestamp >manifests/synapse/bridges/mautrix-telegram/sealedsecret.yaml
-kubeseal -f secrets/synapse/bridges/mautrix-discord/secret.yaml -o yaml | grep -v creationTimestamp >manifests/synapse/bridges/mautrix-discord/sealedsecret.yaml
-kubeseal -f secrets/synapse/bridges/registrations/secret.yaml -o yaml | grep -v creationTimestamp >manifests/synapse/bridges/registrations/sealedsecret.yaml
-kubeseal -f secrets/redis/secret.yaml -o yaml | grep -v creationTimestamp >manifests/redis/sealedsecret.yaml
-kubeseal -f secrets/immich/secret.yaml -o yaml | grep -v creationTimestamp >manifests/immich/sealedsecret.yaml
-kubeseal -f secrets/inadyn/secret.yaml -o yaml | grep -v creationTimestamp >manifests/inadyn/sealedsecret.yaml
-kubeseal -f secrets/cert-manager/secret.yaml -o yaml | grep -v creationTimestamp >manifests/cert-manager/sealedsecret.yaml
-kubeseal -f secrets/nextcloud/secret.yaml -o yaml | grep -v creationTimestamp >manifests/nextcloud/sealedsecret.yaml
-kubeseal -f secrets/postgres/secret.yaml -n nextcloud -o yaml | grep -v creationTimestamp >>manifests/nextcloud/sealedsecret.yaml
-kubeseal -f secrets/synapse/matrix-synapse.yaml -o yaml | grep -v creationTimestamp >manifests/synapse/sealedsecret.yaml
-kubeseal -f secrets/synapse/matrix-synapse-signingkey.yaml -o yaml | grep -v creationTimestamp >>manifests/synapse/sealedsecret.yaml
-kubeseal -f secrets/synapse/postgres-synapse.yaml -o yaml | grep -v creationTimestamp >>manifests/synapse/sealedsecret.yaml
-kubeseal -f secrets/synapse/redis-password.yaml -o yaml | grep -v creationTimestamp >>manifests/synapse/sealedsecret.yaml
-kubeseal -f secrets/synapse/sliding-sync.yaml -o yaml | grep -v creationTimestamp >>manifests/synapse/sealedsecret.yaml
-kubeseal -f secrets/velero/credentials-velero.yaml -o yaml | grep -v creationTimestamp >manifests/velero/sealedsecret.yaml
-kubeseal -f secrets/velero/velero-repo-credentials.yaml -o yaml | grep -v creationTimestamp >>manifests/velero/sealedsecret.yaml
+function encrypt {
+    echo "Encrypting $1 to $2"
+    kubeseal -f $1 -o yaml | grep -v creationTimestamp >$2
+}
+encrypt secrets/authentik/secret.yaml manifests/authentik/sealedsecret.yaml
+encrypt secrets/synapse/bridges/mautrix-signal/secret.yaml manifests/synapse/bridges/mautrix-signal/sealedsecret.yaml
+encrypt secrets/synapse/bridges/mautrix-gmessages/secret.yaml manifests/synapse/bridges/mautrix-gmessages/sealedsecret.yaml
+encrypt secrets/synapse/bridges/mautrix-telegram/secret.yaml manifests/synapse/bridges/mautrix-telegram/sealedsecret.yaml
+encrypt secrets/synapse/bridges/mautrix-discord/secret.yaml manifests/synapse/bridges/mautrix-discord/sealedsecret.yaml
+encrypt secrets/synapse/bridges/registrations/secret.yaml manifests/synapse/bridges/registrations/sealedsecret.yaml
+encrypt secrets/redis/secret.yaml manifests/redis/sealedsecret.yaml
+encrypt secrets/immich/secret.yaml manifests/immich/sealedsecret.yaml
+encrypt secrets/inadyn/secret.yaml manifests/inadyn/sealedsecret.yaml
+encrypt secrets/cert-manager/secret.yaml manifests/cert-manager/sealedsecret.yaml
+encrypt secrets/nextcloud/secret.yaml manifests/nextcloud/sealedsecret.yaml
+kubeseal -f secrets/postgres/secret.yaml -o yaml -n nextcloud | grep -v creationTimestamp >>manifests/nextcloud/sealedsecret.yaml
+encrypt secrets/synapse/matrix-synapse-signingkey.yaml manifests/synapse/sealedsecret.yaml
+encrypt secrets/synapse/postgres-synapse.yaml manifests/synapse/sealedsecret.yaml
+encrypt secrets/synapse/redis-password.yaml manifests/synapse/sealedsecret.yaml
+encrypt secrets/velero/credentials-velero.yaml manifests/velero/sealedsecret.yaml
+encrypt secrets/velero/velero-repo-credentials.yaml manifests/velero/sealedsecret.yaml
