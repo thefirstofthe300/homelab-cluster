48de9c298edc71c9c60b1b9a3eced74180485e5a
Danny Seymour <danny@seymour.family>
2025-12-12 23:19:38 -0800

    fix: Upgrade flux and sealed secrets

--- hack/bootstrap.sh
+++ hack/bootstrap.sh
@@ -1,9 +1,4 @@
 #! /usr/bin/env bash
 
-flux bootstrap github \
-  --owner=thefirstofthe300 \
-  --repository=homelab-cluster \
-  --interval=10m0s \
-  --components-extra=image-reflector-controller,image-automation-controller \
-  --path=manifests/ \
-  --personal
+flux install --export \
+  --components-extra=image-reflector-controller,image-automation-controller > manifests/flux-system/gotk-components.yaml
