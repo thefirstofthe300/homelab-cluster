baec199a2317df2803ce90d8887eccbafba5c809
Danny Seymour <danny@seymour.family>
2026-03-21 17:10:34 -0700

    feat: add dyn-a-go to cluster

--- hack/generate-secrets.sh
+++ hack/generate-secrets.sh
@@ -11,6 +11,7 @@ encrypt secrets/synapse/bridges/mautrix-discord/secret.yaml manifests/synapse/br
 encrypt secrets/synapse/bridges/registrations/secret.yaml manifests/synapse/bridges/registrations/sealedsecret.yaml
 # encrypt secrets/redis/secret.yaml manifests/redis/sealedsecret.yaml
 encrypt secrets/immich/secret.yaml manifests/immich/sealedsecret.yaml
+encrypt secrets/dyn-a-go/secret.yaml manifests/dyn-a-go/sealedsecret.yaml
 # encrypt secrets/inadyn/secret.yaml manifests/inadyn/sealedsecret.yaml
 # encrypt secrets/cert-manager/secret.yaml manifests/cert-manager/sealedsecret.yaml
 # encrypt secrets/nextcloud/secret.yaml manifests/nextcloud/sealedsecret.yaml
